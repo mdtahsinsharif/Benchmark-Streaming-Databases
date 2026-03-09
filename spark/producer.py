74ecc7b5351bb8db0afb8a9953a38f9e4076f1f9
Tahsin Sharif <Md.Tahsin.Sharif@ibm.com>
2026-03-09 01:27:11 -0400

    add flink latency and throughput calculations
    add percentiles
    watermark timestamps
    increased parallelism with kafka configs

--- spark/producer.py
+++ spark/producer.py
@@ -1,24 +1,35 @@
 import json
 import time
-import random
 from kafka import KafkaProducer
+import random
 
 producer = KafkaProducer(
     bootstrap_servers="localhost:9092",
-    value_serializer=lambda v: json.dumps(v).encode("utf-8")
+
+    # serialization
+    value_serializer=lambda v: json.dumps(v).encode(),
+
+    # batching
+    batch_size=65536,
+    linger_ms=5,
+
+    # compression increases throughput
+    compression_type="lz4",
+
+    # large buffers
+    buffer_memory=67108864
 )
 
-regions = ["US", "EU", "ASIA"]
+regions = ["north", "south", "east", "west"]
 
 while True:
-    data = {
-        "order_id": random.randint(1, 1000000),
-        "amount": random.randint(50, 500),
+
+    now = int(time.time() * 1000)
+
+    event = {
         "region": random.choice(regions),
-        "event_time": int(time.time() * 1000)
+        "amount": random.randint(1,100),
+        "event_time": now
     }
 
-    producer.send("sales", data)
-    print("Sent:", data)
-
-    time.sleep(0.05)   # control throughput rate
+    producer.send("sales", event)
